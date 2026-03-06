098df4d14236330a0fd67da524d5de5d70071297
Raffael Brauner <raffael.brauner@outlook.com>
2026-03-06 17:10:29 +0100

    Projektdateien hinzufügen.

---
+++ FolderSorter.Lib/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSorter.Lib
+{
+    public class Class1
+    {
+        string _name;
+
+        public string GetName
+        {
+            set
+            {
+                string temp = value.ToLower();
+                _name = temp;
+            }
+            get { return _name; }
+        }
+
+        public Class1(string name)
+        {
+            GetName = name;
+        }
+    }
+}
---
+++ FolderSorter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FolderSorter.Lib;
+
+namespace FolderSorter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 Hlib = new Class1("Hlib");
+
+            Console.WriteLine(Hlib.GetName);
+        }
+    }
+}
